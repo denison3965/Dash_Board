bdd1d1d89ce0a852e5c021cccf09ff31b99d50e1
Denison <denisondealmeidaprtela@hotmail.com>
2020-07-19 01:25:05 -0300

    Implementando filtro do Java scritp

---
+++ assets/script/script.js
@@ -0,0 +1,383 @@
+const meusDados = [
+    {
+        "nome": "Fernando",
+        "Temperatura_do_quarto": 19,
+        "Temperatura_do_paciênte": 34,
+        "Frequência_cardíaca": 130,
+        "Nível_de_oxigênio": 92,
+        "Pressão_arterial": 82,
+        "Data": "13.07.2020"
+      }, {
+        "nome": "Gabriela",
+        "Temperatura_do_quarto": 19,
+        "Temperatura_do_paciênte": 35,
+        "Frequência_cardíaca": 74,
+        "Nível_de_oxigênio": 93,
+        "Pressão_arterial": 99,
+        "Data": "13.07.2020"
+      }, {
+        "nome": "Ricardo",
+        "Temperatura_do_quarto": 20,
+        "Temperatura_do_paciênte": 36,
+        "Frequência_cardíaca": 100,
+        "Nível_de_oxigênio": 98,
+        "Pressão_arterial": 94,
+        "Data": "13.07.2020"
+      }, {
+        "nome": "Isadora",
+        "Temperatura_do_quarto": 20,
+        "Temperatura_do_paciênte": 38,
+        "Frequência_cardíaca": 74,
+        "Nível_de_oxigênio": 97,
+        "Pressão_arterial": 110,
+        "Data": "13.07.2020"
+      }, 
+      
+    
+    
+    
+      {
+        "nome": "Fernando",
+        "Temperatura_do_quarto": 21,
+        "Temperatura_do_paciênte": 38,
+        "Frequência_cardíaca": 136,
+        "Nível_de_oxigênio": 94,
+        "Pressão_arterial": 109,
+        "Data": "14.07.2020"
+      }, {
+        "nome": "Gabriela",
+        "Temperatura_do_quarto": 20,
+        "Temperatura_do_paciênte": 36,
+        "Frequência_cardíaca": 59,
+        "Nível_de_oxigênio": 97,
+        "Pressão_arterial": 83,
+        "Data": "14.07.2020"
+      }, {
+        "nome": "Ricardo",
+        "Temperatura_do_quarto": 20,
+        "Temperatura_do_paciênte": 38,
+        "Frequência_cardíaca": 137,
+        "Nível_de_oxigênio": 91,
+        "Pressão_arterial": 103,
+        "Data": "14.07.2020"
+      }, {
+        "nome": "Isadora",
+        "Temperatura_do_quarto": 22,
+        "Temperatura_do_paciênte": 36,
+        "Frequência_cardíaca": 103,
+        "Nível_de_oxigênio": 90,
+        "Pressão_arterial": 114,
+        "Data": "14.07.2020"
+      }, 
+      
+      
+      
+      
+      
+      
+      
+      {
+        "nome": "Fernando",
+        "Temperatura_do_quarto": 20,
+        "Temperatura_do_paciênte": 39,
+        "Frequência_cardíaca": 60,
+        "Nível_de_oxigênio": 97,
+        "Pressão_arterial": 106,
+        "Data": "15.07.2020"
+      }, {
+        "nome": "Gabriela",
+        "Temperatura_do_quarto": 22,
+        "Temperatura_do_paciênte": 37,
+        "Frequência_cardíaca": 128,
+        "Nível_de_oxigênio": 95,
+        "Pressão_arterial": 118,
+        "Data": "15.07.2020"
+      }, {
+        "nome": "Ricardo",
+        "Temperatura_do_quarto": 17,
+        "Temperatura_do_paciênte": 34,
+        "Frequência_cardíaca": 86,
+        "Nível_de_oxigênio": 90,
+        "Pressão_arterial": 90,
+        "Data": "15.07.2020"
+      }, {
+        "nome": "Isadora",
+        "Temperatura_do_quarto": 21,
+        "Temperatura_do_paciênte": 38,
+        "Frequência_cardíaca": 69,
+        "Nível_de_oxigênio": 96,
+        "Pressão_arterial": 97,
+        "Data": "15.07.2020"
+      }, 
+      
+      
+      
+      
+      
+      
+      {
+        "nome": "Fernando",
+        "Temperatura_do_quarto": 20,
+        "Temperatura_do_paciênte": 36,
+        "Frequência_cardíaca": 59,
+        "Nível_de_oxigênio": 98,
+        "Pressão_arterial": 120,
+        "Data": "16.07.2020"
+      }, {
+        "nome": "Gabriela",
+        "Temperatura_do_quarto": 18,
+        "Temperatura_do_paciênte": 35,
+        "Frequência_cardíaca": 64,
+        "Nível_de_oxigênio": 90,
+        "Pressão_arterial": 114,
+        "Data": "16.07.2020"
+      }, {
+        "nome": "Ricardo",
+        "Temperatura_do_quarto": 17,
+        "Temperatura_do_paciênte": 39,
+        "Frequência_cardíaca": 72,
+        "Nível_de_oxigênio": 99,
+        "Pressão_arterial": 105,
+        "Data": "16.07.2020"
+      }, {
+        "nome": "Isadora",
+        "Temperatura_do_quarto": 22,
+        "Temperatura_do_paciênte": 37,
+        "Frequência_cardíaca": 101,
+        "Nível_de_oxigênio": 94,
+        "Pressão_arterial": 98,
+        "Data": "16.07.2020"
+      }, 
+      
+      
+      
+      {
+        "nome": "Fernando",
+        "Temperatura_do_quarto": 21,
+        "Temperatura_do_paciênte": 36,
+        "Frequência_cardíaca": 145,
+        "Nível_de_oxigênio": 94,
+        "Pressão_arterial": 110,
+        "Data": "17.07.2020"
+      }, {
+        "nome": "Gabriela",
+        "Temperatura_do_quarto": 21,
+        "Temperatura_do_paciênte": 34,
+        "Frequência_cardíaca": 148,
+        "Nível_de_oxigênio": 97,
+        "Pressão_arterial": 89,
+        "Data": "17.07.2020"
+      }, {
+        "nome": "Ricardo",
+        "Temperatura_do_quarto": 17,
+        "Temperatura_do_paciênte": 34,
+        "Frequência_cardíaca": 80,
+        "Nível_de_oxigênio": 98,
+        "Pressão_arterial": 96,
+        "Data": "17.07.2020"
+      }, {
+        "nome": "Isadora",
+        "Temperatura_do_quarto": 18,
+        "Temperatura_do_paciênte": 36,
+        "Frequência_cardíaca": 115,
+        "Nível_de_oxigênio": 92,
+        "Pressão_arterial": 103,
+        "Data": "17.07.2020"
+      }, 
+      
+      
+      
+      
+      
+      {
+        "nome": "Fernando",
+        "Temperatura_do_quarto": 18,
+        "Temperatura_do_paciênte": 38,
+        "Frequência_cardíaca": 120,
+        "Nível_de_oxigênio": 98,
+        "Pressão_arterial": 82,
+        "Data": "18.07.2020"
+      }, {
+        "nome": "Gabriela",
+        "Temperatura_do_quarto": 24,
+        "Temperatura_do_paciênte": 39,
+        "Frequência_cardíaca": 149,
+        "Nível_de_oxigênio": 91,
+        "Pressão_arterial": 110,
+        "Data": "18.07.2020"
+      }, {
+        "nome": "Ricardo",
+        "Temperatura_do_quarto": 21,
+        "Temperatura_do_paciênte": 37,
+        "Frequência_cardíaca": 146,
+        "Nível_de_oxigênio": 96,
+        "Pressão_arterial": 103,
+        "Data": "18.07.2020"
+      }, {
+        "nome": "Isadora",
+        "Temperatura_do_quarto": 17,
+        "Temperatura_do_paciênte": 36,
+        "Frequência_cardíaca": 84,
+        "Nível_de_oxigênio": 100,
+        "Pressão_arterial": 84,
+        "Data": "18.07.2020"
+      }, 
+      
+      
+      
+      
+      
+      
+      {
+        "nome": "Fernando",
+        "Temperatura_do_quarto": 21,
+        "Temperatura_do_paciênte": 39,
+        "Frequência_cardíaca": 118,
+        "Nível_de_oxigênio": 91,
+        "Pressão_arterial": 84,
+        "Data": "19.07.2020"
+      }, {
+        "nome": "Gabriela",
+        "Temperatura_do_quarto": 21,
+        "Temperatura_do_paciênte": 37,
+        "Frequência_cardíaca": 116,
+        "Nível_de_oxigênio": 93,
+        "Pressão_arterial": 94,
+        "Data": "19.07.2020"
+      }, {
+        "nome": "Ricardo",
+        "Temperatura_do_quarto": 23,
+        "Temperatura_do_paciênte": 37,
+        "Frequência_cardíaca": 140,
+        "Nível_de_oxigênio": 100,
+        "Pressão_arterial": 96,
+        "Data": "19.07.2020"
+      }, {
+        "nome": "Isadora",
+        "Temperatura_do_quarto": 23,
+        "Temperatura_do_paciênte": 34,
+        "Frequência_cardíaca": 100,
+        "Nível_de_oxigênio": 94,
+        "Pressão_arterial": 107,
+        "Data": "19.07.2020"
+      }
+]
+
+let fernando = []
+
+fernando = criarDadosDoPaciente("Fernando", 6 )
+
+console.log(fernando)
+
+
+//Metodo para filtra meus dados que vem dos sensores e me retornar os dados que eu preciso de cada paciênte que eu quiser
+//Modo de uso :
+//  1 Argumento = o Nome do Paciente em que queremos pegar os dados
+//  2 Argumento = o numero do tipo de dados que eu quero que retorne
+//
+//        1 - para retornar temperatura do quarto
+//        2 - para retornar temperatura do paciente
+//        3 - para retornar frequencia cardiaca
+//        4 - para retornar o nivel do oxigenio
+//        5 - para retornar a pressao arterial
+//        6 - para retornar a data
+
+function criarDadosDoPaciente (nome, dado) 
+{
+    let paciente1 = []
+    let temperaturaDoQuarto1 = []
+    let TemperaturaDoPaciênte1 = []
+    let frequenciaCardiaca1 = []
+    let nivelDeOxigenio1 = []
+    let pressaoArteria1 = []
+    let data1 = []
+
+    let infASerMandado = null
+
+    switch (dado) {
+        case 1:
+            infASerMandado = temperaturaDoQuarto1
+            break;
+        case 2:
+            infASerMandado = TemperaturaDoPaciênte1
+            break;    
+        case 3:
+            infASerMandado = frequenciaCardiaca1
+            break;            
+        case 4:
+            infASerMandado = nivelDeOxigenio1
+            break;   
+        case 5:
+            infASerMandado = pressaoArteria1
+            break;  
+        case 6:
+            infASerMandado = data1
+            break;          
+
+        default:
+            return undefined
+            break;
+    }
+
+
+    console.log(nome)
+
+    meusDados.forEach( element => {
+        if (element.nome == nome) 
+        paciente1.push(element) 
+
+    });
+
+
+
+    paciente1.forEach(element => {
+        temperaturaDoQuarto1.push(element.Temperatura_do_quarto)   
+        
+        
+    });
+
+    paciente1.forEach(element => {
+        TemperaturaDoPaciênte1.push(element.Temperatura_do_paciênte)   
+        
+        
+    });
+
+    paciente1.forEach(element => {
+        frequenciaCardiaca1.push(element.Frequência_cardíaca)   
+        
+        
+    });
+
+    paciente1.forEach(element => {
+        nivelDeOxigenio1.push(element.Nível_de_oxigênio)   
+        
+        
+    });
+
+    paciente1.forEach(element => {
+        pressaoArteria1.push(element.Pressão_arterial)   
+        
+        
+    });
+
+    paciente1.forEach(element => {
+        data1.push(element.Data)   
+        
+        
+    });
+
+    return infASerMandado
+
+    
+
+
+}
+
+
+
+
+
+
+
+
+
